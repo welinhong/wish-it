4c408f4518895eccf9995331e6b4b61e9fe01454
Welin Hong <danbi@healingpaper.com>
2021-09-13 08:19:45 +0900

    add LoginPage

---
+++ pages/login.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import styled from 'styled-components'
+
+const LoginPage: NextPage = () => {
+  return <StyledLoginPage>Login</StyledLoginPage>
+}
+
+const StyledLoginPage = styled.div``
+
+export default LoginPage
