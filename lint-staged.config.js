282210b4cd2072cac4c3b1e3db2d182fc1532f9d
Welin Hong <danbi@healingpaper.com>
2021-09-20 09:17:08 +0900

    bash 명령어로 실행할 때 전체 파일을 체크하는 이슈가 있어 커밋 전 타입 체킹하는 방법 수정 - 임시 tsconfig을 생성하고 이 파일 include 속성에 staged files만 넣어 타입 체킹을 한다

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,12 @@
+const fs = require('fs')
+
 module.exports = {
-  '**/*.(ts|tsx)': 'bash -c tsc --noEmit',
+  '**/*.(ts|tsx)': (stagedFilenames) => {
+    const tsconfig = JSON.parse(fs.readFileSync('tsconfig.json', 'utf8'))
+    tsconfig.include = stagedFilenames
+    fs.writeFileSync('tsconfig.lint.json', JSON.stringify(tsconfig))
+    return 'tsc --noEmit --project tsconfig.lint.json'
+  },
 
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': ['npx eslint --fix', 'npx prettier --write'],
