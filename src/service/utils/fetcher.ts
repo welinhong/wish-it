3cc43fb6ed35cb62f84edf3dfe1c4ed0271e6dbc
Welin Hong <danbi@healingpaper.com>
2021-09-23 22:05:31 +0900

    add utils/fetcher

---
+++ src/service/utils/fetcher.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse, Method } from 'axios'
+
+interface FetcherParams {
+  url: string
+  method: Method
+  data?: unknown
+  params?: unknown
+  config?: { [key: string]: unknown }
+  baseURL?: string
+  headers?: unknown
+}
+
+export const fetcher = async <T>({
+  url,
+  method,
+  data = null,
+  params = null,
+  baseURL = '',
+  headers,
+  config,
+}: FetcherParams): Promise<T> => {
+  const result: AxiosResponse<T> = await axios({
+    method,
+    url,
+    data,
+    params,
+    baseURL,
+    headers,
+    ...config,
+  })
+
+  return result.data
+}
