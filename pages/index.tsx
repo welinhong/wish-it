255d00912dd9a4de334696fd2263a79fd4d4ed95
Welin Hong <danbi@healingpaper.com>
2021-09-27 20:25:16 +0900

    불필요한 코드 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,7 @@
-import Modal from '@/components/Modal/Modal'
 import type { NextPage } from 'next'
-import { useState } from 'react'
 
 const Home: NextPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const handleClick = () => {
-    setIsModalOpen((prev) => !prev)
-  }
-  const handleCloseModal = () => {
-    setIsModalOpen(false)
-  }
-
-  return (
-    <>
-      Home
-      <button onClick={handleClick}>show modal</button>
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <div>웰린짱</div>
-      </Modal>
-    </>
-  )
+  return <>Home</>
 }
 
 export default Home
