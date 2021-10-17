7b80223e7dfbe3c6b9521970b81d31cce36a2a2a
Welin Hong <danbi@healingpaper.com>
2021-10-17 20:41:42 +0900

    마우스 hover 했을 때, 상품 정보가 뜬다

--- src/components/WishItemWithContent/index.tsx
+++ src/components/WishItemWithContent/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Typography from '@/components/Typography'
 
 export interface Props {
   title: string
@@ -11,16 +12,86 @@ export interface Props {
 const WishItemWithContent = (props: Props): JSX.Element => {
   const { title, price, siteUrl, photoUrl, giverName } = props
 
-  return <S.Container>content</S.Container>
+  return (
+    <SContainer>
+      {photoUrl ? (
+        <SPhoto src={photoUrl} alt={title} />
+      ) : (
+        <SOnlyTitle>
+          <h2>{title}</h2>
+        </SOnlyTitle>
+      )}
+
+      <SInformation>
+        <Typography type="header2" as="h2" marginBottom="8px" ellipsis>
+          {title}
+        </Typography>
+        <Typography type="body2" as="p" marginBottom="8px" ellipsis>
+          {price}원
+        </Typography>
+        <Typography type="body2" color="primary" as="p" marginBottom="8px" ellipsis>
+          {getOrigin(siteUrl)}
+        </Typography>
+        <Typography type="header3" as="h3" marginBottom="8px" ellipsis>
+          by {giverName}
+        </Typography>
+      </SInformation>
+    </SContainer>
+  )
 }
 
-const S = {
-  Container: styled.div`
-    width: 100%;
-    height: 100%;
-    border-radius: inherit;
-    background-color: #f2f4f6;
-  `,
+const getOrigin = (url: string) => {
+  return new URL(url).origin
 }
+
+const SPhoto = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const SOnlyTitle = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #000000;
+  h2 {
+    background: linear-gradient(106.45deg, #1e50ff 0%, #abff4d 98.75%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`
+const SInformation = styled.div`
+  display: none;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  padding: 25px 20px;
+  background: rgba(0, 0, 0, 0.6);
+  backdrop-filter: blur(4px);
+  color: #ffffff;
+  overflow: hidden;
+`
+const SContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  border-radius: inherit;
+  border: 1px solid #f2f4f6;
+  background-color: #f2f4f6;
+  cursor: pointer;
+  &:hover {
+    ${SInformation} {
+      display: block;
+    }
+  }
+  > * {
+    border-radius: inherit;
+  }
+`
 
 export default WishItemWithContent
