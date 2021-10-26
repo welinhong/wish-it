7431e0f57e6f9ca937275256435bd7e106280fd7
Welin Hong <danbi@healingpaper.com>
2021-10-27 08:19:32 +0900

    url을 통해 해당 사이트의 meta 정보 가져오는 메서드 추가

--- src/utils/urlScraper.ts
+++ src/utils/urlScraper.ts
@@ -1,14 +1,18 @@
 import { fetcher } from './../service/utils/fetcher'
 
-export const getOgTagFromUrl = async (
-  url: string,
-): Promise<{ [key: string]: string } | undefined> => {
+type OpTag = {
+  title?: string
+  image?: string
+  description?: string
+  type?: string
+}
+export const getOgTagFromUrl = async (url: string): Promise<OpTag> => {
   // url 호출해서 html 불러오기
   const html = await fetcher<string>({
     url,
     method: 'GET',
   })
-  if (!html) return
+  if (!html) return {}
 
   // html로부터 meta tag 가져오기
   const metaTags = parseHTML(html)?.getElementsByTagName('meta')
@@ -16,13 +20,18 @@ export const getOgTagFromUrl = async (
   // og 태그만 필터링
   const metaInfos = Array.from(metaTags)
   const openGraphes = metaInfos.filter((meta) => {
-    return Array.from(meta.attributes).findIndex((attr) => attr.value.includes('og')) > -1
+    return (
+      Array.from(meta.attributes).findIndex((attr) =>
+        attr.value.includes('og'),
+      ) > -1
+    )
   })
   return openGraphes.reduce((prev, cur) => {
     const [property, content] = Array.from(cur.attributes)
+    const key = property?.value.replace('og:', '')
     return {
       ...prev,
-      [property?.value]: content?.value,
+      [key]: content?.value,
     }
   }, {})
 }
